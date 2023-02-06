59fa7eae46a5fc4c3ace83a53e999a2ce9b41b6f
DylanDogeNTI <dylan.hoang@elev.ga.ntig.se>
2023-02-06 21:38:09 +0100

    Initial project structure

---
+++ app.rb
@@ -0,0 +1,11 @@
+require 'sinatra'
+require 'sinatra/reloader'
+require 'slim'
+require 'sqlite3'
+require 'bcrypt'
+require_relative './model.rb'
+enable :sessions
+
+get('/') do
+    slim(:index)
+end
