4188909205cafb00a52aa9b9cc0eee3716981583
DylanDogeNTI <dylan.hoang@elev.ga.ntig.se>
2023-04-10 02:36:54 +0200

    Database and FK
    *Components can be created and can be updated by admins
    *pcList has FK on userID
    *user update and read
    *3 levels of authorization:
    guest, user, admin features
    *Route auth check
    
    CSS

--- app.rb
+++ app.rb
@@ -14,10 +14,10 @@ $db = SQLite3::Database.new("db/products.db")
 $db.results_as_hash = true
 
 class DBexecutor
-    def insertIntoPCList(name, time, author_id)
+    def insertIntoPCList(name, time, author_id, components)
         $db.execute("INSERT INTO computerList(computerName, creationTime, author_id) VALUES(?,?,?)", name, time, author_id)
         pcID = $db.execute("SELECT last_insert_rowid()")[0]["last_insert_rowid()"]
-        return insertIntoPCRelation(pcID)
+        return insertIntoPCRelation(pcID, components)
     end
 
     def insertIntoPCRelation(pcID, components)
@@ -26,7 +26,11 @@ class DBexecutor
         end
     end
 
-    def readPCList()
+    def readPCList(id)
+        return $db.execute("SELECT * FROM computerList WHERE id = ?", id)
+    end
+
+    def readPCLists()
         return $db.execute("SELECT * FROM ((computerList_component_rel
             INNER JOIN computerList ON computerList_component_rel.computerList_id = computerList.id)
             INNER JOIN products ON computerList_component_rel.product_id = products.id)")
@@ -42,12 +46,16 @@ class DBexecutor
         $db.execute("DELETE FROM computerList WHERE id = ?", id)
     end
 
+    def readProduct(id)
+        return $db.execute("SELECT * FROM products WHERE id = ?", id)
+    end
+
     def readAllProducts(category)
         return $db.execute("SELECT * FROM products WHERE category = ?", category)
     end
 
     def readUserInfo(username)
-        return $db.execute("SELECT id, password FROM users WHERE username = ?", username)
+        return $db.execute("SELECT id, password, isAdmin FROM users WHERE username = ?", username)
     end
 
     def registerUser(username, password_digest, time, isAdmin)
@@ -62,22 +70,75 @@ class DBexecutor
     def updateComputer(name, id)
         return $db.execute("UPDATE computerList SET computerName = ? WHERE id = ?", name, id)
     end
+
+    def updateComponent(name, desc, category, id)
+        return $db.execute("UPDATE products SET name = ?, desc = ?, category = ? WHERE id = ?", name, desc, category, id)
+    end
+
+    def readAllUsers()
+        return $db.execute("SELECT username, id, creationTime, isAdmin FROM users")
+    end
+    
+    def readUser(id)
+        return $db.execute("SELECT id, username, creationTime, isAdmin FROM users WHERE id = ?", id)
+    end
+
+    def updateUsername(id, username)
+        return $db.execute("UPDATE users SET username = ? WHERE id = ?", username, id)
+    end
+
+    def deleteUser(id)
+        $db.execute("")
+    end
+end
+
+def isLoggedIn()
+    if session[:user_id].nil?
+        redirect('/login/')
+    end
+end
+
+def authorizedUser(authorizedUserID)
+    if session[:user_id] == nil
+        redirect('/login/')
+    elsif session[:user_id] != authorizedUserID
+        denied("#{params[:id]} that is owned by User #{authorizedUserID}")
+        redirect('/lists/')
+    end
+end
+
+def denied(message)
+    puts "Access Denied: User #{session[:user_id]} tried to access #{message}"
+end
+
+def trueAdmin(userID)
+    user = DBexecutor.new.readUser(userID)
+    return user[0]["isAdmin"] == 1
 end
 
 get('/') do
     slim(:home)
 end
 
-get('/lists') do
-    @computers = DBexecutor.new.readPCList()
+before('/lists/') do
+    isLoggedIn()
+end
+
+get('/lists/') do
+    @computers = DBexecutor.new.readPCLists()
+    @users = DBexecutor.new.readAllUsers()
     @formatedComputers = []
     for computer in @computers
-        if !@formatedComputers.include?({"computerName"=>computer["computerName"], "id"=>computer["computerList_id"]})
-            @formatedComputers.append({"computerName"=>computer["computerName"], "id"=>computer["computerList_id"]}) 
+        if !@formatedComputers.include?({"computerName"=>computer["computerName"], "id"=>computer["computerList_id"], "author_id"=>computer["author_id"]})
+            @formatedComputers.append({"computerName"=>computer["computerName"], "id"=>computer["computerList_id"], "author_id"=>computer["author_id"]}) 
         end
     end
 
     slim(:"computerLists/index")
+end
+
+before('/lists/new') do
+    isLoggedIn()
 end
 
 get('/lists/new') do
@@ -87,8 +148,15 @@ get('/lists/new') do
         @categories.append(DBexecutor.new.readAllProducts(partType))
     end
     
-    p @categories
     slim(:"computerLists/new")
+end
+
+before('/lists/:id/edit') do
+    isLoggedIn()
+    if session[:isAdmin] == false && !trueAdmin(session[:user_id])
+        authorID = DBexecutor.new.readPCList(params[:id])
+        authorizedUser(authorID[0]["author_id"])
+    end
 end
 
 get('/lists/:id/edit') do
@@ -116,6 +184,14 @@ get('/lists/:id/edit') do
     slim(:"computerLists/edit")
 end
 
+before('/lists/:id/update') do
+    isLoggedIn()
+    if session[:isAdmin] == false && !trueAdmin(session[:user_id])
+        authorID = DBexecutor.new.readPCList(params[:id])
+        authorizedUser(authorID[0]["author_id"])
+    end
+end
+
 post('/lists/:id/update') do
     components = [
         params[:cpu_id],
@@ -134,7 +210,11 @@ post('/lists/:id/update') do
         components
     )
 
-    redirect('/lists')
+    redirect('/lists/')
+end
+
+before('/lists/:id') do
+    isLoggedIn()
 end
 
 get('/lists/:id') do
@@ -143,10 +223,14 @@ get('/lists/:id') do
     slim(:"computerLists/show")
 end
 
+before('/lists') do
+    isLoggedIn()
+end
+
 post('/lists') do    
-    author_id = params[:author_id]
-    if params[:author_id] == nil
-        author_id = 0
+    author_id = session[:user_id]
+    if author_id == nil
+        redirect('/login/')
     end
 
     components = [
@@ -163,38 +247,56 @@ post('/lists') do
         author_id,
         components
     )
-    redirect('/lists')
+    redirect('/lists/')
+end
+
+before('/lists/:id/delete') do
+    isLoggedIn()
+    if session[:isAdmin] == false && !trueAdmin(session[:user_id])
+        authorID = DBexecutor.new.readPCList(params[:id])
+        authorizedUser(authorID[0]["author_id"])
+    end
 end
 
 post('/lists/:id/delete') do
     listID = params[:id]
     DBexecutor.new.deletePCList(listID)
-    redirect('/lists')
-
+    redirect('/lists/')
 end
 
-get('/login') do
+get('/login/') do
     slim(:login)
 end
 
-get('/register') do 
+get('/register/') do 
     slim(:register)
 end
 
-get('/logout') do
+get('/logout/') do
     session.destroy
     redirect('/')
 end
 
 post('/login') do
     result = DBexecutor.new.readUserInfo(params[:username])
+    if result == []
+        redirect('/login/')
+    end
+    
     user_id = result.first["id"]
     password_digest = result.first["password"]
     if BCrypt::Password.new(password_digest) == params[:password]
         session[:user_id] = user_id
         p "ID!!! = "
         p session[:user_id]
-        redirect('/lists')
+
+        if result.first["isAdmin"] == 1
+            session[:isAdmin] = true
+        else
+            session[:isAdmin] = false
+        end
+
+        redirect('/lists/')
     else
         redirect('/')
     end
@@ -210,12 +312,104 @@ post('/register') do
         if password == passwordConfirm
             password_digest = BCrypt::Password.create(password)
             DBexecutor.new.registerUser(params[:username], password_digest, Time.new().to_i, 0)
-            redirect('/')
+            redirect('/login/')
         else
             redirect('/')
         end
     else
         redirect('/')
     end
+
+end
+
+get('/components/') do
+    @partTypes = ['CPU', 'GPU', 'RAM']
+    @categories = []
+    for partType in @partTypes
+        @categories.append(DBexecutor.new.readAllProducts(partType))
+    end
+    slim(:'components/index')
+end
+
+get('/components/:id') do
+    @product = DBexecutor.new.readProduct(params[:id])
+    slim(:'components/show')
+end
+
+before('/components/:id/edit') do
+    isLoggedIn()
+    if session[:isAdmin] == false && !trueAdmin(session[:user_id])
+        redirect('/components/')
+    end
+end
+
+get('/components/:id/edit') do
+    @partTypes = ['CPU', 'GPU', 'RAM']
+    @product = DBexecutor.new.readProduct(params[:id])
+    slim(:'components/edit')
+end
+
+before('/components/:id/update') do
+    isLoggedIn()
+    if session[:isAdmin] == false && !trueAdmin(session[:user_id])
+        redirect('/components/')
+    end
+end
+
+post('/components/:id/update') do
+    DBexecutor.new.updateComponent(
+        params[:name],
+        params[:desc], 
+        params[:category],
+        params[:id]
+    )
+
+    redirect('/components/')
+end
+
+before('/user/:id/edit') do
+    isLoggedIn()
+    if session[:isAdmin] == false && !trueAdmin(session[:user_id])
+        authorizedUser(params[:id].to_i)
+    end
+end
+
+get('/user/:id/edit') do
+    @user = DBexecutor.new.readUser(params[:id])
+    slim(:'user/edit')
+end
+
+before('/user/:id/update') do
+    isLoggedIn()
+    if session[:isAdmin] == false && !trueAdmin(session[:user_id])
+        authorizedUser(params[:id].to_i)
+    end
+end
+
+post('/user/:id/update') do
+    DBexecutor.new.updateUsername(params[:id], params[:username])
+    redirect('/lists/')
+end
+
+before('/user/') do
+    isLoggedIn()
+    if session[:isAdmin] == false && !trueAdmin(session[:user_id])
+        redirect('/lists/')
+    end
+end
+
+get('/user/') do
+    @users = DBexecutor.new.readAllUsers()
+    slim(:'user/index')
+end
+
+before('/user/:id/delete') do
+    isLoggedIn()
+    if session[:isAdmin] == false && !trueAdmin(session[:user_id])
+        redirect('/lists/')
+    end
+end
+
+post('/user/:id/delete') do
 
 end
